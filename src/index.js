13a3af49adbd8bca8a0da09d8b2b7116a26c2429
Viktoiya Mikhaylova <viktoriya.mikhaylova@hotmail.com>
2023-05-04 23:38:06 +0300

    commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,18 @@
 import React from "react";
-import ReactDOM from 'react-dom/client';
+import { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
 
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 
+const container = document.getElementById("root");
+const root = createRoot(container);
+
 // render BrowserRouter
-const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
+  <BrowserRouter>
     <App />
+  </BrowserRouter>
 );
 
 serviceWorker.unregister();
-
-
-
-
